9d7313d7c8ad277fb252799df1ceba883453afde
Rahul Dahal <rdaahal@gmail.com>
2021-12-31 18:26:39 +0545

    chore: initialize express server dotenv and helmet

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
-function sayHello(name) {
-  console.log(`Hello ${name}`);
-}
+import express from 'express';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
 
-sayHello('Rahul');
+// initialize dotenv to use variables defined on /.env
+dotenv.config();
+
+// initialize express server
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(helmet());
+
+app.listen(PORT, ()=>console.log(`The server is listining on port ${PORT}`));
