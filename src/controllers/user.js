0bbac92a548d1a5c19ecf7a043081494a8667c73
Rahul Dahal <rdaahal@gmail.com>
2022-01-02 17:42:52 +0545

    feat: create new user
    refactor: user controller to use User Model

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,4 +1,12 @@
-export function signUp(req, res){
+import { createUser } from "../models/User";
+
+export async function signUp(req, res){
     const {email, password} = req.body;
-    res.status(201).json({message: `user is created with email ${email}`});
+    const {error, message} = await createUser({email, password});
+    if(error){
+        const {reason, errorMessage} = error;
+        const statusCode = reason === 'clientError' ? 400 : 500;
+        return res.status(statusCode).json({message: {error: errorMessage}});
+    }
+    return res.status(201).json({message});
 }
