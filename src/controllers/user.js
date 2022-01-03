79c38c28b552f583958a3bc982d3773d4bd53a90
Rahul Dahal <rdaahal@gmail.com>
2022-01-03 10:26:03 +0545

    chore: format code

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,12 +1,12 @@
-import { createUser } from "../models/User";
+import { createUser } from '../models/User';
 
-export async function signUp(req, res){
-    const {email, password} = req.body;
-    const {error, message} = await createUser({email, password});
-    if(error){
-        const {reason, errorMessage} = error;
-        const statusCode = reason === 'clientError' ? 400 : 500;
-        return res.status(statusCode).json({message: {error: errorMessage}});
-    }
-    return res.status(201).json({message});
+export async function signUp(req, res) {
+  const { email, password } = req.body;
+  const { error, message } = await createUser({ email, password });
+  if (error) {
+    const { reason, errorMessage } = error;
+    const statusCode = reason === 'clientError' ? 400 : 500;
+    return res.status(statusCode).json({ message: { error: errorMessage } });
+  }
+  return res.status(201).json({ message });
 }
