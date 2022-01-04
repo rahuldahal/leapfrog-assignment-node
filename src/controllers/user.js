6d91d4c89216871f8009e3aeea2b25e196216205
Rahul Dahal <rdaahal@gmail.com>
2022-01-04 19:33:58 +0545

    feat: signIn controller

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,4 +1,4 @@
-import { createUser } from '../models/User';
+import { createUser, signInUser } from '../models/User';
 
 export async function signUp(req, res) {
   const { email, password } = req.body;
@@ -9,4 +9,15 @@ export async function signUp(req, res) {
     return res.status(statusCode).json({ message: { error: errorMessage } });
   }
   return res.status(201).json({ message });
+}
+
+export async function signIn(req, res) {
+  const { email, password } = req.body;
+  const { error, message } = await signInUser({ email, password });
+  if (error) {
+    const { reason, errorMessage } = error;
+    const statusCode = reason === 'clientError' ? 400 : 500;
+    return res.status(statusCode).json({ message: { error: errorMessage } });
+  }
+  return res.status(200).json({ message });
 }
