36cb7eb2dee04a25bfb4a363748aedad523dca30
Rahul Dahal <rdaahal@gmail.com>
2022-01-13 12:23:45 +0545

    feat: edit contact logic

--- src/controllers/contact.js
+++ src/controllers/contact.js
@@ -1,4 +1,4 @@
-import { create } from '../models/Contact';
+import { create, edit } from '../models/Contact';
 
 export async function createContact(req, res, next) {
   const { name, phone } = req.body; // TODO: add photograph as well
@@ -11,4 +11,16 @@ export async function createContact(req, res, next) {
   const { _id } = message; // _id of new contact; will be referenced on user doc.
   req.contactId = _id;
   next();
+}
+
+export async function editContact(req, res) {
+  const { name, phone } = req.body;
+  const { contactId: _id } = req;
+  const { message, error } = await edit({ _id, name, phone });
+  if (error) {
+    const { reason, errorMessage } = error;
+    const statusCode = reason === 'clientError' ? 400 : 500;
+    return res.status(statusCode).json({ message: { error: errorMessage } });
+  }
+  return res.status(201).json({ message });
 }
--- src/models/Contact/index.js
+++ src/models/Contact/index.js
@@ -14,3 +14,20 @@ export async function create({ name, phone }) {
     return returnValue;
   }
 }
+
+export async function edit({ _id, name, phone }) {
+  // TODO: add photograph as well
+  const returnValue = { error: null, message: null };
+  try {
+    const updatedContact = await Contact.findOneAndUpdate(
+      { _id },
+      { name, phone },
+      { new: true }
+    );
+    returnValue.message = { updatedContact };
+    return returnValue;
+  } catch (error) {
+    returnValue.error = errorHandler(error);
+    return returnValue;
+  }
+}
